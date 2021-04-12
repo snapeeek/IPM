7853006df77591d28555c6776cfd50ee8eca412f
snapeeek <maciekwozniak97@gmail.com>
2021-04-12 11:08:14 +0200

    Added creating table :)

--- IPM5/script.js
+++ IPM5/script.js
@@ -14,7 +14,7 @@ if (!window.indexedDB) {
 
 const clientData = [
     {id: "00-01", name: "gopal", surname: "gogo", email: "gopal@tutorialspoint.com"},
-    {id: "00-02", name: "prasad", surname:"tudu", email: "prasad@tutorialspoint.com"}
+    {id: "00-02", name: "prasad", surname: "tudu", email: "prasad@tutorialspoint.com"}
 ];
 var db;
 var request = window.indexedDB.open("newDatabase", 1);
@@ -57,17 +57,18 @@ function read() {
 }
 
 function readAll() {
+    generateTableHead();
     var objectStore = db.transaction("client").objectStore("client");
 
     objectStore.openCursor().onsuccess = function (event) {
         var cursor = event.target.result;
-
-        if (cursor) {
-            alert("Name for id " + cursor.key + " is " + cursor.value.name + " " + cursor.value.surname + ", Email: " + cursor.value.email);
-            cursor.continue();
-        } else {
-            alert("No more entries!");
-        }
+        generateTable(cursor);
+    //     if (cursor) {
+    //         alert("Name for id " + cursor.key + " is " + cursor.value.name + " " + cursor.value.surname + ", Email: " + cursor.value.email);
+    //         cursor.continue();
+    //     } else {
+    //         alert("No more entries!");
+    //     }
     };
 }
 
@@ -75,8 +76,10 @@ function add() {
 
     var request = db.transaction(["client"], "readwrite")
         .objectStore("client")
-        .add({id: "00-03", name: document.getElementById('name').value, surname: document.getElementById('surname').value,
-            email: document.getElementById('email').value});
+        .add({
+            id: "00-03", name: document.getElementById('name').value, surname: document.getElementById('surname').value,
+            email: document.getElementById('email').value
+        });
 
     request.onsuccess = function (event) {
         alert("Kenny has been added to your database.");
@@ -96,3 +99,52 @@ function remove() {
         alert("Kenny's entry has been removed from your database.");
     };
 }
+
+
+// ----------------------- Creating table --------------------------
+function generateTableHead() {
+    const data = ["ID", "NAME", "SURNAME", "EMAIL"];
+    const table = document.getElementById('dblist');
+    console.log("plz tell me u work")
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+    for (let key of data) {
+        let th = document.createElement("th");
+        let text = document.createTextNode(key);
+        th.appendChild(text);
+        row.appendChild(th);
+    }
+}
+
+function generateTable(cursor) {
+    const table = document.getElementById('dblist');
+    while (true)
+    {
+        if (cursor)
+        {
+            let row = table.insertRow();
+            let cell = row.insertCell();
+            let text = document.createTextNode(cursor.key);
+            cell.appendChild(text);
+
+            cell = row.insertCell();
+            text = document.createTextNode(cursor.value.name);
+            cell.appendChild(text);
+
+            cell = row.insertCell();
+            text = document.createTextNode(cursor.value.surname);
+            cell.appendChild(text);
+
+            cell = row.insertCell();
+            text = document.createTextNode(cursor.value.email);
+            cell.appendChild(text);
+            cursor.continue();
+        }
+        else
+            break;
+    }
+}
+
+
+
+
