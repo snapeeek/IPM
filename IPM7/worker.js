b6b1bc37266a48a05d26fb2ba6c2924a1822813d
snapeeek <maciekwozniak97@gmail.com>
2021-05-06 21:16:31 +0200

    caseSwapper added

--- IPM7/worker.js
+++ IPM7/worker.js
@@ -1,3 +1,30 @@
 onmessage = function (e) {
-    console.log(JSON.parse(e.data));
+    var tempJSON = JSON.parse(e.data);
+    for (var i = 0; i < tempJSON.length; i++)
+    {
+        tempJSON[i]["name"] = caseSwapper(tempJSON[i]["name"]);
+        tempJSON[i]["surname"] =  caseSwapper(tempJSON[i]["surname"]);
+        tempJSON[i]["phone"] =  caseSwapper(tempJSON[i]["phone"]);
+        tempJSON[i]["email"] =  caseSwapper(tempJSON[i]["email"]);
+    }
+
+    console.log(tempJSON)
 };
+
+function caseSwapper(string)
+{
+    var word = []
+    for (i = 0; i < string.length; i++)
+    {
+        if (string[i] === string[i].toUpperCase())
+        {
+            word[i] = string[i].toLowerCase();
+        }
+        else
+        {
+            word[i] = string[i].toUpperCase();
+        }
+    }
+
+    return word.join("");
+}
